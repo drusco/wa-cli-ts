9a068ece256ea4295d72ad2df7ec279fbe07f5ae
Pedro Gallardo <pedrogallardo7011@gmail.com>
2024-10-23 03:13:19 -0300

    feat: add function to load dicts json file

--- cli.ts
+++ cli.ts
@@ -1,3 +1,6 @@
+import { readFileSync } from "fs";
+import path from "path";
+
 interface Item {
   name: string;
   items?: Item[];
@@ -6,3 +9,11 @@ interface Item {
 interface Data {
   data: Item[];
 }
+
+function loadData(): Data {
+  const filePath = path.resolve("dicts/data.json");
+  const jsonData = readFileSync(filePath, "utf-8");
+  return JSON.parse(jsonData);
+}
+
+console.log(loadData());
