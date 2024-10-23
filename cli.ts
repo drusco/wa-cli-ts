9f596ac2b52f12fefe9c71abb76534b6a95a88cc
Pedro Gallardo <pedrogallardo7011@gmail.com>
2024-10-23 03:21:07 -0300

    feat: add cli app setup

--- cli.ts
+++ cli.ts
@@ -10,10 +10,34 @@ interface Data {
   data: Item[];
 }
 
+const VALID_ACTIONS = ["analyze"];
+
 function loadData(): Data {
   const filePath = path.resolve("dicts/data.json");
   const jsonData = readFileSync(filePath, "utf-8");
   return JSON.parse(jsonData);
 }
 
-console.log(loadData());
+async function app(): Promise<void> {
+  const args = process.argv.slice(2);
+
+  if (args.length === 0 || !VALID_ACTIONS.includes(args[0])) {
+    console.log(
+      'usage:\n\r\tanalyze --depth <n> --verbose (optional) "{phrase}"'
+    );
+    return;
+  }
+
+  const action = args[0];
+  const actionArgs = args.slice(1);
+
+  if (action === "analyze") {
+    await analyze(actionArgs);
+  }
+}
+
+async function analyze(args: string[]): Promise<void> {
+  console.log(args);
+}
+
+app();
